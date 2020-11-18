d7fc9bb8c1f9ad7829551a2216a7b93fb475c896
unknown <joliete@edu.tecnocampus.cat>
2020-11-18 20:56:40 +0100

    Udpate when input changes and state changes made

--- generador.py
+++ generador.py
@@ -3,20 +3,24 @@ import dash_core_components as dcc
 import dash_html_components as html
 import networkx as nx
 import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 from random import random
+from random import seed
+from random import choice
+import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__,  external_stylesheets=external_stylesheets)
 
 def escampa(G):
-  for n in G.nodes:
-    if G.nodes[n]['infectat']:
-      # busquem els veins
-      for v in G[n]:
-        if random() > contagirate :
-          G.nodes[v]['infectat']=True
-          G.nodes[v]['color']="red"
-  return (G)
+        for n in G.nodes:
+            if G.nodes[n]['infectat']:
+                # busquem els veins
+                for v in G[n]:
+                    if random() > contagirate :
+                        G.nodes[v]['infectat']=True
+                        G.nodes[v]['color']="red"
+        return (G)
 
 def create_graph_plot(G):
     edge_x = []
@@ -54,49 +58,61 @@ def create_graph_plot(G):
             line_width=0))
     return (edge_trace,node_trace)
 
+#Variables maximes
+contagirate=1
+diesmax=15
+contagiratemax=100
+vacinespercentagemax=100
+novacinespercentagemax=100
+quarantinedpopulationmax=100
 
+#Generem aleatoriament el graph
 G= nx.dual_barabasi_albert_graph(2000,2,1,0.1)
 pos = nx.spring_layout(G, k=0.25, iterations=20)
 nx.set_node_attributes(G,pos,'pos')
 
 nx.set_node_attributes(G,"blue",'color')
 nx.set_node_attributes(G,False,'infectat')
+nx.set_node_attributes(G,False,'vacunat')
+nx.set_node_attributes(G,True,'vacunable')
 G.nodes[300]['color']="red"
 G.nodes[300]['infectat']=True
 
-# taula amb les colors a posar en segons el dia
+#MODIFICACIONS DEL GRAPH ABANS DE GENERAR ESTATS
+
+#Primer eliminar aletoriament els nodes que estan en quarentena del graph depenent del input del usuari (exemple input 33)
+seed(1)
+sequenceeliminate = [i for i in range(G.nodes*33/100)]
+for x in sequenceeliminate:
+    G.remove_node(x)
+
+#Aqui cambiar l'estat dels nodes per 'novacunable' aleatoriament depenen del % del input del usuari (exemple input 33)
+sequencenovaccinate = [i for i in range(G.nodes*33/100)]
+for x in sequencenovaccinate:
+    G.nodes[i]['vacunable']=False
+    G.nodes[i]['color']="green"
+
+#Depenenet del anterior posar aleatoriament els nodes l'estat 'vacunat' depenen del % del input
+sequencevaccinate = [i for i in range(G.nodes*33/100)]
+for x in sequencevaccinate:
+    if G.nodes[i]['color']!='green':
+        G.nodes[i]['vacunat']=True
+        G.nodes[i]['color']="green"
+
 estats=[]
-contagirate=0.95
-diesmax=30
-contagiratemax=100
-vacinespercentagemax=100
-novacinespercentagemax=100
-quarantinedpopulationmax=100
-
-# poso el dia 0
+# posem un exemple del graph amb nomes un infectat
 estats.append(nx.get_node_attributes(G,"color"))
-for i in range(1,diesmax) :
-  G=escampa(G)  
-  # poso a estats el color del node segons el dia 
-  estats.append(nx.get_node_attributes(G,"color"))
-
-
-
 nx.set_node_attributes(G,estats[0],'color')
-# Hi ha parts del create_graph_plot que no varien
 fig = go.Figure(data=create_graph_plot(G))
 
 app.layout = html.Div(children=[
     html.H1(children='Evolució covid'),
-
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
-
-
     dcc.Graph(
-        id='xarxa',
-        figure=fig
+    id='xarxa',
+    figure=fig
     ),
     html.Div(children='''
         Day
@@ -163,32 +179,39 @@ app.layout = html.Div(children=[
             value=0,
             step=1
             ), 
-        style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
+        style={'width': '49%', 'padding': '0px 20px 20px 20px'})
 ])
-
-
-@app.callback(
-    dash.dependencies.Output('contagirate-slider', 'children'),
-    [dash.dependencies.Input('contagirate-slider', 'value')])
-
-def update_output(value):
-    return 'Heu seleccionat una taxa de contagi de {}'.format(value)
-
-def update_graph(value):
-    nx.set_node_attributes(G,estats[value],'color')
-    fig = go.Figure(data=create_graph_plot(G))
-    # retornem la figura, que s'ha de substituir
-    return fig
-
 
 # indiquem que el output d'aquest callback es la figure del component amb id xarxa
 # # el callback te com a input un canvi en el dia--slider
 @app.callback(dash.dependencies.Output('xarxa', 'figure'),
-
-            [ dash.dependencies.Input('dia-slider', 'value')
+            [ dash.dependencies.Input('dia-slider', 'value'),
+            dash.dependencies.Input('contagirate-slider', 'value'),
+            dash.dependencies.Input('quarantinedpopulation-slider', 'value'),
+            dash.dependencies.Input('novacinespercentage-slider', 'value'),
+            dash.dependencies.Input('vacinespercentage-slider', 'value')
             ])
+def update_graph(dia, contagirate, quarantine, novacines, vacines):
 
-def update_graph(dia):
+    def escampa(G):
+        for n in G.nodes:
+            if G.nodes[n]['infectat']:
+                # busquem els veins
+                for v in G[n]:
+                    if random() > 1-contagirate :
+                        if G.nodes[v]['vacunat']==True:
+                        else:
+                            G.nodes[v]['infectat']=True
+                            G.nodes[v]['color']="red"
+        return (G)
+    
+    #Generem estats 
+    estats=[]
+    for i in range(1,diesmax) :
+        G=escampa(G)
+        # poso a estats el color del node segons el dia 
+        estats.append(nx.get_node_attributes(G,"color"))
+
     nx.set_node_attributes(G,estats[dia],'color')
     fig = go.Figure(data=create_graph_plot(G))
     # retornem la figura, que s'ha de substituir
